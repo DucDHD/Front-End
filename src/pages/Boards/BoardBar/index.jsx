3e6a2106a70d8f4bc06a33c28c0d726c04509891
Hoang Duc <duonghoangduc1325@gmail.com>
2026-03-20 00:00:54 +0700

    board content

--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -37,8 +37,8 @@ function BoardBar() {
       gap: 2,
       overflowX: 'auto',
       bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
-      borderBottom: '1px solid white',
-      paddingX: 2
+      paddingX: 2,
+      '&::-webkit-scrollbar-track': { m: 2 }
     }}>
       <Box sx={{ display:'flex', alignItems: 'center', gap: 2 }}>
         <Chip sx={MENU_STYLES}
