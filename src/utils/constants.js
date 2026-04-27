aa3f8eeafc89ba57ef5d359bed748d6d348bc09e
Hoang Duc <duonghoangduc1325@gmail.com>
2026-04-27 12:34:52 +0700

    update deploy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,11 @@
-export const API_ROOT = 'http://localhost:8017'
+let apiROOT = ''
+
+if (process.env.BUILD_MODE === 'dev') {
+  apiROOT = 'http://localhost:8017'
+}
+
+if (process.env.BUILD_MODE === 'production') {
+  apiROOT = 'https://back-end-6lug.onrender.com'
+}
+
+export const API_ROOT = apiROOT
